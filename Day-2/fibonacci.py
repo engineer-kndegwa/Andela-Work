f8606ad1fe832a09c62bc4899b00a0f113773252
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-06-15 08:18:08 +0300

    Adding the Fibonacci series

--- Day-2/fibonacci.py
+++ Day-2/fibonacci.py
@@ -1,1 +1,13 @@
+def fibon(n):
+   if n == 1:
+      return 1
+   elif n == 0:   
+      return 0 
+   #Using recursion           
+   else:                      
+      return fibon(n-1) + fibon(n-2)
 
+#Validation test for first 5 NUmbers in the series
+#------------------------------------------------
+for i in range(0, 5):
+    print(fibon(i))
