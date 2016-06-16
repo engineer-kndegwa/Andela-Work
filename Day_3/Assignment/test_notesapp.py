a10dd24b8a00da846afd81698237546b8c548a84
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-06-16 08:38:46 +0300

    Changes to the Notes Application

--- Day_3/Assignment/test_notesapp.py
+++ Day_3/Assignment/test_notesapp.py
@@ -2,10 +2,23 @@ import unittest
 from notesapplication import NotesApplication
 
 class TestNotesApp(unittest.TestCase):
+	"""Beginning the test methods"""
 	def test_single_note(self):
 		"""Test that a single note is stored properly"""
-		test_notesApp = NotesApplication(')
+		test_notesApp = NotesApplication('')
 		test_notesApp.create('Four')
 		self.assertIn('Four',test_notesApp.notes_list)
+	def test_if_note_id_is_an_integer(self):
+		"""Testing if the note id is indeed not an integer"""
+		test_if_int = NotesApplication('')
+		concl = test_if_int.get_note_id(1)
+		self.assertEqual(type(concl),int)
+		"""This is an expected failure because it was type cast to a string"""
+	def test_list_content_not_empty(self):
+		"""This tests if the notes_list is empty or not"""
+		test_if_content= NotesApplication('Dummy Content')
+		test_list_content_if_empty = test_if_content.list_content()
+		self.assertNotEqual(test_list_content_if_empty, None)
+	
 	
 unittest.main()
