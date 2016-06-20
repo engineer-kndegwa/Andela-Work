59b1421bd5365dc79dc1c40e34b48e67074a71ff
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-06-20 12:09:55 +0300

    New Route for API prime numbers

--- Day_6/hello.py
+++ Day_6/hello.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def hundred_primes():
 	return str(isPrime(10))
 	
+	
+@app.route('/<int:n>',methods = ['GET'])
+
+def get_first_n_primes(n):
+	return str(isPrime(n))
+	
 if __name__ =="__main__":
 	app.run(debug=True)
